77ad0afb8ebf7fda75e148af11abc4df83641ffb
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-01-25 20:37:58 +0545

    Min Max

--- 012_Greedy_Algorithm/003_Min_Max.js
+++ 012_Greedy_Algorithm/003_Min_Max.js
@@ -4,11 +4,13 @@ function maxMin(k, arr) {
   let unfaireness;
 
   for (let i = 0; i <= arr.length - k; i++) {
-    let members = arr.slice(i, i + k);
+    // let members = arr.slice(i, i + k);
+    // console.log(members);
+    // console.log(members[0], members[k - 1]);
+    // console.log(arr[i], arr[i + k - 1]);
 
-    console.log(members);
-
-    let tmp = Math.max(...members) - Math.min(...members);
+    // let tmp = Math.max(...members) - Math.min(...members);
+    let tmp = arr[i + k - 1] - arr[i];
     if (i == 0) unfaireness = tmp;
 
     if (tmp < unfaireness) {
@@ -20,5 +22,5 @@ function maxMin(k, arr) {
 }
 
 console.log(maxMin(3, [100, 200, 300, 350, 400, 401, 402])); //2
-console.log(maxMin(4, [1, 2, 3, 4, 10, 20, 30, 40, 100, 200]));
+// console.log(maxMin(4, [1, 2, 3, 4, 10, 20, 30, 40, 100, 200]));
 // console.log(maxMin(3, [10, 100, 300, 200, 1000, 20, 30]));
